c435cf919ec92ba48cd191e6206a4620608f96ee
Chris Clark <chris@chrisclark.uk>
2025-12-02 14:28:46 +0000

    fix: build process

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 echo Building NWN2 Save Reminder...
+echo Downloading dependencies...
+go mod tidy
 go mod download
+echo Building executable...
 go build -o nwn2-save-reminder.exe
 if %ERRORLEVEL% EQU 0 (
     echo Build successful! Run nwn2-save-reminder.exe to start.
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module nwn2-save-reminder
 
 go 1.21
 
-require (
-	github.com/fsnotify/fsnotify v1.7.0
-)
+require github.com/fsnotify/fsnotify v1.7.0
 
+require golang.org/x/sys v0.4.0 // indirect
